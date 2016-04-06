b3783989b9a862babc87211a37ae75311423fefe
Simon Fransson <simon@funnel.io>
2016-04-06 10:58:47 +0200

    Initial Commit

---
+++ Airport Terminal/Airport_TerminalView.h
@@ -0,0 +1,13 @@
+//
+//  Airport_TerminalView.h
+//  Airport Terminal
+//
+//  Created by Simon Fransson on 06/04/16.
+//  Copyright © 2016 Simon Fransson. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Airport_TerminalView : ScreenSaverView
+
+@end
---
+++ Airport Terminal/Airport_TerminalView.m
@@ -0,0 +1,52 @@
+//
+//  Airport_TerminalView.m
+//  Airport Terminal
+//
+//  Created by Simon Fransson on 06/04/16.
+//  Copyright © 2016 Simon Fransson. All rights reserved.
+//
+
+#import "Airport_TerminalView.h"
+
+@implementation Airport_TerminalView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
